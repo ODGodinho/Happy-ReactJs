8602f2ac5ca6814e90a5887c355130a83beec57b
ODGodinho <DragonsGamers@hotmail.com.br>
2020-10-17 16:54:28 -0300

    fix(): cors fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,10 +11,10 @@ import errorHandler from "../Exceptions/Handler";
 const app = express();
 
 // habilita o uso de Json no express
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler)
-app.use(cors())
 
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
