6ca970d943d880997aa650c00eb79150560346f6
ODGodinho <DragonsGamers@hotmail.com.br>
2020-10-20 20:10:32 -0300

    chore(): Header in routes

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -5,22 +5,53 @@ import { createStackNavigator } from "@react-navigation/stack";
 import OrphanagesMap from "./pages/OrphanagesMap";
 import OrphanageDetails from "./pages/OrphanageDetails";
 
+import OrphanageData from "./pages/createOrphanage/OrphanageData";
+import SelectMapPosition from "./pages/createOrphanage/SelectMapPosition";
+import Header from "./components/Header";
+
 const { Navigator, Screen } = createStackNavigator();
 
 export default function routes() {
     return (
         <NavigationContainer>
             <Navigator screenOptions={{
-                headerShown: false
-            }}>
+                headerShown: false,
+                cardStyle: {
+                    backgroundColor: "#F2F3F5"
+                }
+            }}
+            >
                 <Screen
-                    name="OrphanageMap"
+                    name="OrphanagesMap"
                     component={OrphanagesMap}
                 ></Screen>
+
                 <Screen
                     name="OrphanageDetails"
                     component={OrphanageDetails}
                 ></Screen>
+
+
+                {/* Create Orphanage */}
+                <Screen
+                    name="SelectMapPosition"
+                    component={SelectMapPosition}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCancel={false} title="Selecione no mapa" />
+                    }}
+                ></Screen>
+
+                <Screen
+                    name="OrphanageData"
+                    component={OrphanageData}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Informe os Dados" />
+                    }}
+                ></Screen>
+
+
             </Navigator>
         </NavigationContainer>
     )
